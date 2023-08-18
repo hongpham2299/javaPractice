73be5ff3f8def9e7ae74c16ae3ac7d003d4d1a91
Hong Pham Diaz <hongpham2299@gmail.com>
2023-08-17 23:21:53 -0400

    add arrays homework

---
+++ src/arrays/Main.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        Write a program that prints 1 to 100 by default
+        and prints i to j where i and j are integers
+         */
+
+        PracticeOne practiceOne = new PracticeOne();
+        practiceOne.printNumbers(1, 100);
+        practiceOne.printNumbers(11, 70);
+    }
+}
--- src/arrays/PracticeOne.java
+++ src/arrays/PracticeOne.java
@@ -2,23 +2,6 @@ package arrays;
 
 public class PracticeOne {
 
-    public static void main(String[] args) {
-
-        /*
-        Write a program that prints 1 to 100 by default
-        and prints i to j where i and j are integers
-         */
-        PracticeOne practiceOne = new PracticeOne();
-        practiceOne.printNumbers(3, 110);
-
-    }
-
-    private void printDefaultNumber(){
-        for(int i=1; i<=100; i++){
-            System.out.println(i);
-        }
-    }
-
     public void printNumbers(int i, int j) {
         if (i!= 1 && j!= 100) {
             for (int k = i; k <= j; k++) {
@@ -26,6 +9,12 @@ public class PracticeOne {
             }
         } else {
             printDefaultNumber();
+        }
+    }
+
+    private void printDefaultNumber(){
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
         }
     }
 }
