9248774506e221ccf8a21b33d2bf5aa512456e5c
Hong Pham Diaz <hongpham2299@gmail.com>
2023-08-22 00:46:56 -0400

    update the printNumbers method

--- src/main/java/arrays/PracticeOne.java
+++ src/main/java/arrays/PracticeOne.java
@@ -3,18 +3,14 @@ package arrays;
 public class PracticeOne {
 
     public void printNumbers(int i, int j) {
-        if (i!= 1 && j!= 100) {
-            for (int k = i; k <= j; k++) {
+        if(i<=j){
+            for (int k=i; k<=j; k++){
                 System.out.println(k);
             }
-        } else {
-            printDefaultNumber();
-        }
-    }
-
-    private void printDefaultNumber(){
-        for(int i=1; i<=100; i++){
-            System.out.println(i);
+        } else if (i>j) {
+            for(int k=i; k>=j; k--){
+                System.out.println(k);
+            }
         }
     }
 }
